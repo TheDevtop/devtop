0202f85469b8a57462d3b5d231f62137762a6070
Thijs Haker <theage2001@gmail.com>
2021-04-17 21:41:20 +0200

    0.3: Added logic modules and home directory check.

--- devtop.sh
+++ devtop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Program: DEVTOP
-# Version: 0.2
+# Version: 0.3
 # Authors: Thijs Haker
 
 # Hello World!
@@ -14,11 +14,12 @@ if [[ $(id -u) -ne 0 ]] ; then
 fi
 
 # Check if Xorg is intalled (dependeny)
-printf "Info: Checking dependencies...\n"
+printf "Info: Checking dependencies..."
 if [[ ! -f /usr/bin/X ]] ; then
 	printf "Error: Xorg is not installed!\n"
 	exit 1
 fi
+printf "Done!\n"
 
 # Prompt user for ~ directory
 printf "Path to home directory: " ; read
@@ -28,4 +29,19 @@ if [ -z "$REPLY" ] ; then
 fi
 export HOME=$REPLY
 
+# Check if home exists
+if [ ! -d $HOME  ] ; then
+	printf "Error: Specified directory doesn't exist!\n"
+	exit 1
+fi
+
+# Update database
+printf "Info: Updating database...\n" ; pacman -Sy > /dev/null
+
+# Execute scripts
+printf "Info: Running system logic...\n" ; bash logic/system
+printf "Info: Running user logic...\n" ; bash logic/user
+
+# Goodbye and exit
+printf "Info: Done! Please reboot the machine!\n"
 exit 0
